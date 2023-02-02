85c080a6424fa0c565df8bf459336b8af0de408a
criipi <cdml2001@hotmail.com>
2023-02-02 17:01:49 -0500

    emails are display

--- app/src/main/java/com/example/gmailcloneapp/Email.kt
+++ app/src/main/java/com/example/gmailcloneapp/Email.kt
@@ -1,6 +1,8 @@
 package com.example.gmailcloneapp
 
-class Email(val sender: String,val title: String,val summary: String) {
-
-
+class Email(
+    val sender: String,
+    val title: String,
+    val message: String)
+{
 }
